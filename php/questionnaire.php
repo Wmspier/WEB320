4b771de9edcebf1f8a1d9724937d181d793c4ddb
Wmspier <Wmspier16@gmail.com>
2015-12-15 17:54:16 -0500

    Updated questionnaire

--- php/questionnaire.php
+++ php/questionnaire.php
@@ -1,9 +1,22 @@
 <?php
 
-echo "meow";
+$Adapt,$Friendly,$Needs,$Train;
 
 if(isset($_POST['Q1'])){
     $Q1 = $_POST['Q1'];
+    echo $Q1;
+    switch($Q1){
+        case 1:
+            $Adapt++;
+            break;
+        case 2:
+            break;
+        case 3:
+            $Adapt--;
+            break;
+        default:
+            break;
+    }
 }
 
 
