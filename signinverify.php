c553d686bd43728c4292b63c7c575d099f82f137
Wmspier <Wmspier16@gmail.com>
2015-12-14 19:27:15 -0500

    authentication through sql srvr

--- signinverify.php
+++ signinverify.php
@@ -3,6 +3,9 @@
 //header('Location: main.php');
 session_start();
 
+$con = new mysqli('65.183.130.64','user1','F0n%3','testusers');
+$_SESSION['timeout']=time();
+
 if(isset($_POST['inputFN'])){
     $FN = $_POST['inputFN'];
     $_SESSION['inputFN']=$FN;
@@ -17,12 +20,45 @@ if(isset($_POST['group4'])){
     $type = $_POST['group4'];
     $_SESSION['type']=$type;
     echo "<br>You are logged in as a ";
-   if($type=='user') echo "User";
-   else if($type=='admin') echo "Admin";
-}else{
+   if($type=='user') 
+   {
+	$sql = "SELECT fn,ln FROM Users WHERE fn='$FN' AND ln='$LN'";
+	$result= $con->query($sql);
+        if($result->num_rows == 0)
+		{
+			echo 'Invalid username or password<br>';
+			echo '<form action="signin.html">
+					<button type="submit">Try Again</button>
+				</form>';
+		}
+        else
+		{
+			echo '<h1>Logged in</h1><p>Logged In: ', $FN, '</p>';
+			echo "Admin";
+		}
+	}
+	else if($type=='admin') 
+	{
+	$sql = "SELECT fn,ln FROM Admin WHERE fn='$FN' AND ln='$LN'";
+	$result= $con->query($sql);
+        if($result->num_rows == 0)
+		{
+			echo 'Invalid username or password<br>';
+			echo '<form action="signin.html">
+					<button type="submit">Try Again</button>
+				</form>';
+		}
+        else
+		{
+			echo '<h1>Logged in</h1><p>Logged In: ', $FN, '</p>';
+			echo "Admin";
+		}
+	}
+	else{
     $type='guest';
     $_SESSION['type']=$type;
     echo "<br>You are logged in as a Guest";
+	}
 }
 
 if(isset($_POST['remember-me'])){
@@ -33,7 +69,6 @@ if(isset($_POST['remember-me'])){
     $inactive=3600;
 }
 
-
 if(isset($_SESSION['timeout'])){
     $sessionLifeSpan=time() - $_SESSION['timeout'];
     if($sessionLifeSpan>$inactive){
@@ -43,19 +78,6 @@ if(isset($_SESSION['timeout'])){
     }
 }
 
-$_SESSION['timeout']=time();
-
-
-
-$html = <<< HTML
-    <!DOCTYPE html>
-    <html lang="en">
-    <form action="main.php" method="post">
-    <button type="submit">NEXT</button>
-    </form>
-    </html>
-HTML;
-echo $html;
 
 
 ?>
